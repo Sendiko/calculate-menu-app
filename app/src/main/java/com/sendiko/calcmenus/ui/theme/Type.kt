734363193d96660be97f5ab1b29840925fd296dd
Sendiko <sarangtawon897@gmail.com>
2023-08-08 11:25:56 +0700

    enhancement - add font family

--- app/src/main/java/com/sendiko/calcmenus/ui/theme/Type.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/theme/Type.kt
@@ -2,9 +2,62 @@ package com.sendiko.calcmenus.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sendiko.calcmenus.R
+
+val myFont = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.lato_black,
+            weight = FontWeight.ExtraBold
+        ),
+        Font(
+            resId = R.font.lato_blackitalic,
+            weight = FontWeight.ExtraBold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.lato_bold,
+            weight = FontWeight.ExtraBold
+        ),
+        Font(
+            resId = R.font.lato_bolditalic,
+            weight = FontWeight.ExtraBold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.lato_regular,
+            weight = FontWeight.ExtraBold,
+        ),
+        Font(
+            resId = R.font.lato_regular,
+            weight = FontWeight.ExtraBold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.lato_light,
+            weight = FontWeight.ExtraBold
+        ),
+        Font(
+            resId = R.font.lato_lightitalic,
+            weight = FontWeight.ExtraBold,
+            style = FontStyle.Italic
+        ),
+        Font(
+            resId = R.font.lato_thin,
+            weight = FontWeight.ExtraBold
+        ),
+        Font(
+            resId = R.font.lato_thinitalic,
+            weight = FontWeight.ExtraBold,
+            style = FontStyle.Italic
+        ),
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
