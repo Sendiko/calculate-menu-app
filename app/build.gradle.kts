aae04f83a9c3dd6a19ce55a90bac959d2a4c1f25
Sendiko <sarangtawon897@gmail.com>
2023-08-09 19:56:35 +0700

    enhancement - define material 3 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.navigation:navigation-compose:2.6.0")
     implementation("androidx.navigation:navigation-runtime-ktx:2.6.0")
     testImplementation("junit:junit:4.13.2")
