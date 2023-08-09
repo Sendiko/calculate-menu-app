b1a23152b5e4ad5dc483559e97eb3333b598bff3
Sendiko <sarangtawon897@gmail.com>
2023-08-09 19:56:43 +0700

    enhancement - add icons to the button

--- app/src/main/java/com/sendiko/calcmenus/ui/components/buttons/CustomButtons.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/components/buttons/CustomButtons.kt
@@ -3,6 +3,7 @@ package com.sendiko.calcmenus.ui.components.buttons
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,18 +13,21 @@ import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sendiko.calcmenus.ui.components.buttons.ButtonSize.*
+import com.sendiko.calcmenus.ui.components.buttons.IconInButtonPosition.*
 import com.sendiko.calcmenus.ui.theme.LessGray
 import com.sendiko.calcmenus.ui.theme.NotWhite
 import com.sendiko.calcmenus.ui.theme.PrimaryRed
@@ -33,6 +37,8 @@ import com.sendiko.calcmenus.ui.theme.myFont
 fun PrimaryButton(
     modifier: Modifier = Modifier,
     buttonSize: ButtonSize = REGULAR,
+    icon: ImageVector? = null,
+    iconPosition: IconInButtonPosition = BeforeText,
     text: String,
     onClick: () -> Unit,
 ) {
@@ -44,18 +50,29 @@ fun PrimaryButton(
             contentColor = NotWhite
         ),
         content = {
-            Text(
-                text = text,
-                style = TextStyle(
-                    fontSize = when (buttonSize) {
-                        BIG -> 18.sp
-                        REGULAR -> 14.sp
-                        SMALL -> 12.sp
-                    },
-                    fontFamily = myFont,
-                    fontWeight = FontWeight.Bold
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                if (icon!= null && iconPosition == BeforeText){
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+                Text(
+                    text = text,
+                    style = TextStyle(
+                        fontSize = when (buttonSize) {
+                            BIG -> 18.sp
+                            REGULAR -> 14.sp
+                            SMALL -> 12.sp
+                        },
+                        fontFamily = myFont,
+                        fontWeight = FontWeight.Bold
+                    )
                 )
-            )
+                if (icon != null && iconPosition == AfterText) {
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
         }
     )
 }
@@ -64,6 +81,8 @@ fun PrimaryButton(
 fun OutlineButton(
     modifier: Modifier = Modifier,
     buttonSize: ButtonSize = REGULAR,
+    icon: ImageVector? = null,
+    iconPosition: IconInButtonPosition = BeforeText,
     text: String,
     onClick: () -> Unit,
 ) {
@@ -76,18 +95,29 @@ fun OutlineButton(
             contentColor = PrimaryRed
         ),
         content = {
-            Text(
-                text = text,
-                style = TextStyle(
-                    fontSize = when (buttonSize) {
-                        BIG -> 18.sp
-                        REGULAR -> 14.sp
-                        SMALL -> 12.sp
-                    },
-                    fontFamily = myFont,
-                    fontWeight = FontWeight.Bold
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ){
+                if (icon!= null && iconPosition == BeforeText){
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+                Text(
+                    text = text,
+                    style = TextStyle(
+                        fontSize = when (buttonSize) {
+                            BIG -> 18.sp
+                            REGULAR -> 14.sp
+                            SMALL -> 12.sp
+                        },
+                        fontFamily = myFont,
+                        fontWeight = FontWeight.Bold
+                    )
                 )
-            )
+                if (icon != null && iconPosition == AfterText) {
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
         }
     )
 }
