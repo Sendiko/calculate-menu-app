430fd1f0d3d89932d789e59963b70c59b15de6b9
Sendiko <sarangtawon897@gmail.com>
2023-08-10 16:58:12 +0700

    enhancement - add yellow yellow color

--- app/src/main/java/com/sendiko/calcmenus/ui/theme/Color.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/theme/Color.kt
@@ -7,6 +7,7 @@ val TextColor = Color(0xFF808080)
 val LessGray = Color(0xFF404040)
 val CardBackgroundColor = Color(0xFFE6E6E6)
 val NotWhite = Color(0xFFF4F4F4)
+val Yellowyellow = Color(0xFFDAA520)
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
