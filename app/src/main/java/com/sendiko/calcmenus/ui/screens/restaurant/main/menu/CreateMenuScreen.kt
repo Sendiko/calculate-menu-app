92419c3c5edc5299a564521f109af03ce1b2ab35
Sendiko <sarangtawon897@gmail.com>
2023-09-12 18:49:40 +0700

    enhancement - adjustment on create menu screen

--- app/src/main/java/com/sendiko/calcmenus/ui/screens/restaurant/main/menu/CreateMenuScreen.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/screens/restaurant/main/menu/CreateMenuScreen.kt
@@ -8,48 +8,78 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.RestaurantMenu
-import androidx.compose.material.icons.filled.SignalWifiConnectedNoInternet4
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.SubcomposeAsyncImage
+import androidx.compose.ui.unit.sp
 import com.sendiko.calcmenus.ui.components.appbars.CustomAppBar
 import com.sendiko.calcmenus.ui.components.textfields.OutlinedTextField
 import com.sendiko.calcmenus.ui.components.textfields.TextArea
+import com.sendiko.calcmenus.ui.screens.Routes
 import com.sendiko.calcmenus.ui.theme.NotWhite
 import com.sendiko.calcmenus.ui.theme.PrimaryRed
 import com.sendiko.calcmenus.ui.theme.myFont
 
 @Composable
-fun CreateMenuScreen() {
+fun CreateMenuScreen(
+    onNavigateBack: (route: String) -> Unit,
+    onMenuCreated: () -> Unit
+) {
     Scaffold(
         containerColor = PrimaryRed,
         topBar = {
             CustomAppBar(
                 title = "Add Menu",
                 headerIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.KeyboardArrowLeft,
-                        contentDescription = "Back",
-                        tint = NotWhite
+                    IconButton(
+                        onClick = { onNavigateBack(Routes.RestoDashboardScreen.route) },
+                        content = {
+                            Icon(
+                                imageVector = Icons.Filled.KeyboardArrowLeft,
+                                contentDescription = "Back",
+                                tint = NotWhite
+                            )
+                        }
                     )
                 },
                 trailingIcon = { }
+            )
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            FloatingActionButton(
+                containerColor = PrimaryRed,
+                contentColor = NotWhite,
+                shape = RoundedCornerShape(100),
+                content = {
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = "Add Menu",
+                        style = TextStyle(
+                            fontSize = 20.sp, fontWeight = FontWeight.Black, fontFamily = myFont
+                        )
+                    )
+                },
+                onClick = {
+                    onMenuCreated()
+                }
             )
         }
     ) {
@@ -64,76 +94,79 @@ fun CreateMenuScreen() {
                 )
                 .background(NotWhite)
         ) {
-            Row(
-                modifier = Modifier.padding(8.dp)
-            ) {
-                Box(modifier = Modifier.weight(1f))
-                SubcomposeAsyncImage(
-                    modifier = Modifier
-                        .weight(2f)
-                        .padding(8.dp)
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(5))
-                        .align(Alignment.CenterVertically),
-                    model = "192.168.1.8:8000/storage/images/menu/MbaC4FwN5iRwoSAVgmk3ORcVcsiumkXw93I9Fd9s.jpg",
-                    contentDescription = "null",
-                    loading = {
-                        CircularProgressIndicator(
-                            modifier = Modifier.padding(32.dp),
-                            color = PrimaryRed,
-                            strokeCap = StrokeCap.Butt
+            LazyColumn(
+                content = {
+                    item {
+                        Row(
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            Box(modifier = Modifier.weight(1f))
+                            IconButton(
+                                modifier = Modifier
+                                    .clip(RoundedCornerShape(10))
+                                    .background(Color.Gray)
+                                    .aspectRatio(1f)
+                                    .weight(2f),
+                                onClick = { onMenuCreated() },
+                                content = {
+                                    Icon(
+                                        modifier = Modifier.padding(48.dp).fillMaxSize(),
+                                        imageVector = Icons.Filled.AddAPhoto,
+                                        contentDescription = "Add picture"
+                                    )
+                                }
+                            )
+                            Box(modifier = Modifier.weight(1f))
+                        }
+                    }
+                    item {
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            hint = "Menu title",
+                            isError = false,
+                            textValue = "",
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Filled.RestaurantMenu,
+                                    contentDescription = "Menu"
+                                )
+                            },
+                            onNewValue = {}
                         )
-                    },
-                    error = {
-                        Icon(
-                            imageVector = Icons.Filled.SignalWifiConnectedNoInternet4,
-                            contentDescription = "No Connection"
+                    }
+                    item {
+                        OutlinedTextField(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            hint = "Menu price",
+                            isError = false,
+                            textValue = "",
+                            leadingIcon = {
+                                Text(
+                                    text = "Rp.",
+                                    style = TextStyle(
+                                        fontFamily = myFont,
+                                        fontWeight = FontWeight.ExtraBold
+                                    )
+                                )
+                            },
+                            onNewValue = {}
                         )
-                    },
-                    contentScale = ContentScale.Crop
-                )
-                Box(modifier = Modifier.weight(1f))
-            }
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                hint = "Menu title",
-                isError = false,
-                textValue = "",
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.RestaurantMenu,
-                        contentDescription = "Menu"
-                    )
-                },
-                onNewValue = {}
-            )
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                hint = "Menu price",
-                isError = false,
-                textValue = "",
-                leadingIcon = {
-                    Text(
-                        text = "Rp.",
-                        style = TextStyle(
-                            fontFamily = myFont,
-                            fontWeight = FontWeight.ExtraBold
+                    }
+                    item {
+                        TextArea(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            hint = "Menu description",
+                            textValue = "",
+                            onNewValue = {}
                         )
-                    )
-                },
-                onNewValue = {}
-            )
-            TextArea(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                hint = "Menu description",
-                textValue = "",
-                onNewValue = {}
+                    }
+                }
             )
         }
     }
@@ -142,5 +175,12 @@ fun CreateMenuScreen() {
 @Preview
 @Composable
 fun CreateMenuScreenPrev() {
-    CreateMenuScreen()
+    CreateMenuScreen(
+        onNavigateBack = {
+
+        },
+        onMenuCreated = {
+
+        }
+    )
 }
