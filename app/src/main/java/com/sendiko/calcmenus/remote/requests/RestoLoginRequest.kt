841fd5943619fee20ad74e1bc9c72464126924d3
Sendiko <sarangtawon897@gmail.com>
2023-10-21 16:14:26 +0700

    enhancement - create RestoLoginRequest

---
+++ app/src/main/java/com/sendiko/calcmenus/remote/requests/RestoLoginRequest.kt
@@ -0,0 +1,12 @@
+package com.sendiko.calcmenus.remote.requests
+
+import com.google.gson.annotations.SerializedName
+
+data class RestoLoginRequest(
+
+	@field:SerializedName("password")
+	val password: String? = null,
+
+	@field:SerializedName("email")
+	val email: String? = null
+)
