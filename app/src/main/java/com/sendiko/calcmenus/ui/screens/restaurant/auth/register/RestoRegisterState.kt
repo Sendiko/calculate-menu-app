9c636aa54ac517afe45eba9d729ae55d91419f62
Sendiko <sarangtawon897@gmail.com>
2023-10-21 16:07:34 +0700

    enhancement - create RestoRegisterState

---
+++ app/src/main/java/com/sendiko/calcmenus/ui/screens/restaurant/auth/register/RestoRegisterState.kt
@@ -0,0 +1,16 @@
+package com.sendiko.calcmenus.ui.screens.restaurant.auth.register
+
+import com.sendiko.calcmenus.ui.utils.FailedState
+
+data class RestoRegisterState(
+    val restoName: String = "",
+    val restoAddress: String = "",
+    val restoPhone: String = "",
+    val email: String = "",
+    val password: String = "",
+    val isLoading: Boolean = false,
+    val failedState: FailedState = FailedState(),
+    val isPasswordVisible: Boolean = false,
+    val loginSuccessful: Boolean = false,
+    val registerPart: Int = 1,
+)
