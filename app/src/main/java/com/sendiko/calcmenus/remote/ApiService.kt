3ab90abd97e17ef059125c4f7a098d03a85161af
Sendiko <sarangtawon897@gmail.com>
2023-10-21 16:14:50 +0700

    enhancement - add resto login endpoint

--- app/src/main/java/com/sendiko/calcmenus/remote/ApiService.kt
+++ app/src/main/java/com/sendiko/calcmenus/remote/ApiService.kt
@@ -1,8 +1,10 @@
 package com.sendiko.calcmenus.remote
 
 import com.sendiko.calcmenus.remote.requests.EmployeeLoginRequest
+import com.sendiko.calcmenus.remote.requests.RestoLoginRequest
 import com.sendiko.calcmenus.remote.requests.RestoRegisterRequest
 import com.sendiko.calcmenus.remote.responses.EmployeeLoginResponse
+import com.sendiko.calcmenus.remote.responses.RestoLoginResponse
 import com.sendiko.calcmenus.remote.responses.RestoRegisterResponse
 import retrofit2.Call
 import retrofit2.http.Body
@@ -19,5 +21,10 @@ interface ApiService {
     fun restoRegister(
         @Body restoRegisterRequest: RestoRegisterRequest
     ): Call <RestoRegisterResponse>
+
+    @POST("resto/login")
+    fun restoLogin(
+        @Body  restoLoginRequest: RestoLoginRequest
+    ): Call<RestoLoginResponse>
 
 }
