de321326e35bb5099dc1898d464689d27386fa41
Sendiko <sarangtawon897@gmail.com>
2023-10-21 11:13:40 +0700

    enhancement - create MessageNotificationView

---
+++ app/src/main/java/com/sendiko/calcmenus/ui/components/others/MessageNotificationView.kt
@@ -0,0 +1,42 @@
+package com.sendiko.calcmenus.ui.components.others
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MessageNotificationView(
+    message: String,
+    isVisible: Boolean,
+    action: (() -> Unit?)? = null
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = expandVertically(),
+        exit = shrinkVertically()
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                contentColor = MaterialTheme.colorScheme.onTertiaryContainer
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(16.dp),
+                text = message,
+            )
+        }
+    }
+}
