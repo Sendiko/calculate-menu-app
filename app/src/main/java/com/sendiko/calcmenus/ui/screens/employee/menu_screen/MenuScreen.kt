f29aa8d33c60a20449f807441ee1235feb85758c
Sendiko <sarangtawon897@gmail.com>
2023-11-02 21:11:52 +0700

    ehc(imp): implement state and onevent

--- app/src/main/java/com/sendiko/calcmenus/ui/screens/employee/menu_screen/MenuScreen.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/screens/employee/menu_screen/MenuScreen.kt
@@ -1,7 +1,10 @@
 package com.sendiko.calcmenus.ui.screens.employee.menu_screen
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,29 +26,26 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.SortByAlpha
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sendiko.calcmenus.remote.responses.MenusItem
 import com.sendiko.calcmenus.ui.components.appbars.CustomAppBar
 import com.sendiko.calcmenus.ui.components.buttons.SelectableOutlineButton
 import com.sendiko.calcmenus.ui.components.buttons.SmallOutlineButton
@@ -63,14 +63,22 @@ import com.sendiko.calcmenus.ui.theme.myFont
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MenuScreen(
-    onPlaceOrder: (route: String) -> Unit,
-    onNavigate: (route: String) -> Unit
+    state: MenuScreenState,
+    onEvent: (MenuScreenEvent) -> Unit,
+    onNavigate: (String) -> Unit
 ) {
-    var menuSelection by rememberSaveable {
-        mutableIntStateOf(0)
-    }
-    var selectedMenu = mutableListOf<IntArray>()
-    val lazyState = rememberLazyListState()
+    LaunchedEffect(
+        key1 = state.menuList,
+        block = {
+            when (state.menuList) {
+                emptyList<MenusItem>() -> {
+                    onEvent(MenuScreenEvent.RequestMenuData(state.token))
+                }
+            }
+        }
+    )
+    var foods = state.menuList?.filter { it?.category == "food" }
+    var beverages = state.menuList?.filter { it?.category == "beverage" }
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         containerColor = PrimaryRed,
@@ -91,7 +99,7 @@ fun MenuScreen(
                     )
                 },
                 onClick = {
-                    onPlaceOrder(Routes.EmployeeOrderResumeScreen.route)
+                    onNavigate(Routes.EmployeeOrderResumeScreen.route)
                 }
             )
         },
@@ -136,174 +144,171 @@ fun MenuScreen(
         }
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
-            verticalArrangement = Arrangement.Top
+            modifier = Modifier.padding(it),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Column(
+            // SearchBar
+            OutlinedTextField(
                 modifier = Modifier
                     .fillMaxWidth()
-            ) {
-                Column(
-                    modifier = Modifier.padding(horizontal = 8.dp)
-                ) {
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        hint = "Search",
-                        isError = false,
-                        keyboardOptions = KeyboardOptions().copy(
-                            imeAction = ImeAction.Search
-                        ),
-                        keyboardActions = KeyboardActions(
-                            onSearch = {
-
+                    .padding(horizontal = 8.dp),
+                hint = "Search",
+                isError = false,
+                keyboardOptions = KeyboardOptions().copy(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        when (state.currentPage) {
+                            0 -> {
+                                foods = state.menuList?.filter { menus ->
+                                    menus?.name == state.searchText
+                                }
                             }
-                        ),
-                        textValue = "",
-                        onNewValue = {},
-                        leadingIcon = {
-                            Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+
+                            1 -> {
+                                beverages = state.menuList?.filter { menus ->
+                                    menus?.name == state.searchText
+                                }
+                            }
+                        }
+                    }
+                ),
+                textValue = state.searchText,
+                onNewValue = { value ->
+                    onEvent(MenuScreenEvent.OnSearchInput(value))
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
+                }
+            )
+            // MenuButton
+            Row(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                menuTypeList.forEachIndexed { index, menuType ->
+                    SelectableOutlineButton(
+                        modifier = Modifier.weight(1f),
+                        text = menuType.type,
+                        isSelected = state.currentPage == index,
+                        onClick = {
+                            onEvent(MenuScreenEvent.SwitchPages(index))
                         }
                     )
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
-                        menuTypeList.forEachIndexed { index, menuType ->
-                            SelectableOutlineButton(
-                                modifier = Modifier.weight(1f),
-                                text = menuType.type,
-                                isSelected = menuSelection == index,
-                                onClick = {
-                                    menuSelection = index
-                                }
-                            )
-                        }
-                        IconButton(
-                            onClick = { /*TODO*/ },
-                            content = {
-                                Icon(
-                                    imageVector = Icons.Filled.SortByAlpha,
-                                    contentDescription = "Sort Content"
-                                )
-                            }
+                }
+                IconButton(
+                    onClick = { /*TODO*/ },
+                    content = {
+                        Icon(
+                            imageVector = Icons.Filled.SortByAlpha,
+                            contentDescription = "Sort Content"
                         )
                     }
-                }
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize(1f)
-                        .clip(
-                            RoundedCornerShape(
-                                topStartPercent = 10, topEndPercent = 10
-                            )
-                        )
-                        .background(NotWhite),
-                ) {
-                    this@Column.AnimatedVisibility(
-                        visible = menuSelection == 0
-                    ) {
-                        LazyColumn(
-                            contentPadding = PaddingValues(horizontal = 16.dp),
-                            verticalArrangement = Arrangement.spacedBy(8.dp),
-                            state = lazyState
-                        ) {
-                            item {
-                                Text(
-                                    modifier = Modifier.padding(start = 10.dp, top = 16.dp),
-                                    text = "Menu",
-                                    style = TextStyle(
-                                        fontWeight = FontWeight.Black,
-                                        color = PrimaryRed,
-                                        fontSize = 32.sp,
-                                        fontFamily = myFont
-                                    )
-                                )
-                            }
-                            items(count = 30) {
-                                MenuCard(
-                                    imageUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Dd560SUzCN5UNP_E8hkbJQHaE6%26pid%3DApi&f=1&ipt=d2bc437090590052ad0108f8ea6354466a0738f84e9334c5d7e2eb96c7e5eb4f&ipo=images",
-                                    title = "French fries $it",
-                                    description = "Kentang kualitan tinggi yang di goreng sempurna $it",
-                                    price = "Rp. 12.500",
-                                    amount = 9,
-                                    onPlusClick = {
-
-                                    },
-                                    onMinusClick = {
-
-                                    },
-                                    onButtonClick = {
-
-                                    }
-                                )
-                            }
-                            item {
-                                Spacer(modifier = Modifier.height(FloatingActionButtonDefaults.LargeIconSize * 2))
-                            }
-                        }
-                    }
-                    this@Column.AnimatedVisibility(
-                        visible = menuSelection == 1
-                    ) {
-                        LazyColumn(
-                            contentPadding = PaddingValues(horizontal = 16.dp),
-                            verticalArrangement = Arrangement.spacedBy(8.dp),
-                            state = lazyState
-                        ) {
-                            item {
-                                Text(
-                                    modifier = Modifier.padding(start = 10.dp, top = 16.dp),
-                                    text = "Menu",
-                                    style = TextStyle(
-                                        fontWeight = FontWeight.Black,
-                                        color = PrimaryRed,
-                                        fontSize = 32.sp,
-                                        fontFamily = myFont
-                                    )
-                                )
-                            }
-                            items(count = 30) {
-                                MenuCard(
-                                    imageUrl = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fi.gojekapi.com%2Fdarkroom%2Fgofood-indonesia%2Fv2%2Fimages%2Fuploads%2F7940780d-1f6e-45a9-afee-5bfc8644c9af.jpg&f=1&nofb=1&ipt=096540f75f2332b259c906bb0a5a2c9905adb3cc07fa2910c9fdf1eeffbe44fc&ipo=images",
-                                    title = "Milkmax Rasa $it",
-                                    description = "Susu sapi segar dipadukan dengan $it",
-                                    price = "Rp. 15.000",
-                                    onPlusClick = {
-
-                                    },
-                                    onMinusClick = {
-
-                                    },
-                                    onButtonClick = {
-
-                                    }
-                                )
-                            }
-                            item {
-                                Spacer(modifier = Modifier.height(FloatingActionButtonDefaults.LargeIconSize * 2))
-                            }
-                        }
-                    }
-                }
+                )
             }
+
+            LazyColumn(
+                modifier = Modifier
+                    .clip(
+                        RoundedCornerShape(
+                            topStartPercent = 8,
+                            topEndPercent = 8
+                        )
+                    )
+                    .background(NotWhite)
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(start = 8.dp, end = 8.dp),
+                content = {
+                    item {
+                        AnimatedVisibility(visible = state.isLoading) {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+                    }
+                    item {
+                        AnimatedVisibility(visible = !state.isLoading) {
+                            Text(
+                                modifier = Modifier.padding(start = 10.dp, top = 16.dp),
+                                text = "Menu",
+                                style = TextStyle(
+                                    fontWeight = FontWeight.Black,
+                                    color = PrimaryRed,
+                                    fontSize = 32.sp,
+                                    fontFamily = myFont
+                                )
+                            )
+                        }
+                    }
+                    items(foods!!) { food ->
+                        Spacer(modifier = Modifier.height(8.dp))
+                        AnimatedVisibility(
+                            visible = state.currentPage == 0 && !state.isLoading,
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            if (food != null) {
+                                MenuCard(
+                                    title = food.name.toString(),
+                                    description = food.description.toString(),
+                                    imageUrl = food.imageUrl.toString(),
+                                    price = food.price.toString(),
+                                    amount = state.orderedMenuList.count { amount ->
+                                        amount == food.name!!
+                                    },
+                                    onButtonClick = {
+                                        onEvent(MenuScreenEvent.OnAddMenuToList(food.name!!))
+                                    },
+                                    onMinusClick = {
+                                        onEvent(MenuScreenEvent.OnRemoveMenuFromList(food.name!!))
+                                    },
+                                    onPlusClick = {
+                                        Log.i("MENU_SCREEN_EVENT", "MenuScreen: onPlusClick")
+                                        onEvent(MenuScreenEvent.OnAddMenuToList(food.name!!))
+                                    }
+                                )
+                            }
+                        }
+                    }
+
+                    items(beverages!!) { beverage ->
+                        Spacer(modifier = Modifier.height(8.dp))
+                        AnimatedVisibility(
+                            visible = state.currentPage == 1 && !state.isLoading,
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+                            if (beverage != null) {
+                                MenuCard(
+                                    title = beverage.name.toString(),
+                                    description = beverage.description.toString(),
+                                    price = beverage.price.toString(),
+                                    imageUrl = beverage.imageUrl.toString(),
+                                    amount = state.orderedMenuList.count { amount ->
+                                        amount == beverage.name!!
+                                    },
+                                    onButtonClick = {
+                                        onEvent(MenuScreenEvent.OnAddMenuToList(beverage.name!!))
+                                    },
+                                    onMinusClick = {
+                                        onEvent(MenuScreenEvent.OnRemoveMenuFromList(beverage.name!!))
+                                    },
+                                    onPlusClick = {
+                                        Log.i("MENU_SCREEN_EVENT", "MenuScreen: onPlusClick")
+                                        onEvent(MenuScreenEvent.OnAddMenuToList(beverage.name!!))
+                                    }
+                                )
+                            }
+                        }
+
+                    }
+                }
+            )
+
         }
-    }
-}
-
-@Preview
-@Composable
-fun MenuScreenPrev() {
-    Surface(
-        color = PrimaryRed
-    ) {
-        MenuScreen(
-            onNavigate = {
-
-            },
-            onPlaceOrder = {
-
-            }
-        )
     }
 }
