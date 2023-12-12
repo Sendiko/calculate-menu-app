40306f5e87d6aa675583549a0a7069d1a8975383
Sendiko <sarangtawon897@gmail.com>
2023-12-12 09:56:06 +0700

    enc(add): add on load menu

--- app/src/main/java/com/sendiko/calcmenus/ui/screens/employee/menu_screen/MenuScreenEvent.kt
+++ app/src/main/java/com/sendiko/calcmenus/ui/screens/employee/menu_screen/MenuScreenEvent.kt
@@ -1,11 +1,14 @@
 package com.sendiko.calcmenus.ui.screens.employee.menu_screen
 
+import com.sendiko.calcmenus.remote.responses.MenusItem
+
 sealed interface MenuScreenEvent {
     data class OnSearchInput(val menuItem: String): MenuScreenEvent
-    data class OnAddMenuToList(val menu: String): MenuScreenEvent
-    data class OnRemoveMenuFromList(val menu: String): MenuScreenEvent
+    data class OnAddMenuToList(val menu: MenusItem): MenuScreenEvent
+    data class OnRemoveMenuFromList(val menu: MenusItem): MenuScreenEvent
     data class OnSortToggle(val sortBy: String): MenuScreenEvent
     data class RequestMenuData(val token: String): MenuScreenEvent
     data class SwitchPages(val page: Int): MenuScreenEvent
+    data class OnLoadMenuList(val menuList: List<MenusItem>): MenuScreenEvent
     object OnPlaceOrder: MenuScreenEvent
 }
