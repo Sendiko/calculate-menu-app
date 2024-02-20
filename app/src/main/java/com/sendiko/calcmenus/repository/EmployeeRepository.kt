77b7f7f9f1892e306d7c8aeeaa5723af25f04456
Sendiko <sarangtawon897@gmail.com>
2024-02-20 16:26:05 +0700

    enc(add): post order method

--- app/src/main/java/com/sendiko/calcmenus/repository/EmployeeRepository.kt
+++ app/src/main/java/com/sendiko/calcmenus/repository/EmployeeRepository.kt
@@ -2,6 +2,7 @@ package com.sendiko.calcmenus.repository
 
 import com.sendiko.calcmenus.remote.ApiConfig
 import com.sendiko.calcmenus.remote.requests.EmployeeLoginRequest
+import com.sendiko.calcmenus.remote.requests.PostOrderRequest
 
 class EmployeeRepository {
 
@@ -12,4 +13,6 @@ class EmployeeRepository {
     fun employeeLogout(token: String) = client.employeeLogout(token)
 
     fun getMenu(token: String, restoId: String) = client.getMenu(restoId, token)
+
+    fun postOrder(token: String, request: PostOrderRequest) = client.postOrder(token, request)
 }
